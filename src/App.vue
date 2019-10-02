02d0d3ef57a8973c1a557afac78137eb715c73ec
Gwapa Ako Kunoh <nylael.nilue18181919@gmail.com>
2019-10-02 21:01:25 +0800

    Hasul ka bai

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
-    <Header
-    ></Header>
+    <Header></Header>
     <AppBody></AppBody>
-    <Sidebar
-      v-show="$route.path === '/Dashboard' || $route.path === '/Subjects'|| $route.path === '/PersonalInfo' ? true: false"
-    ></Sidebar>
   </div>
 </template>
 <style scoped lang="scss">
@@ -20,15 +16,13 @@
 }
 </style>
 <script>
-import Sidebar from "router/frame/Sidebar.vue";
 import Header from "router/frame/Header.vue";
 import AppBody from "router/frame/Body.vue";
 export default {
   name: "app",
   components: {
     Header,
-    AppBody,
-    Sidebar
+    AppBody
   }
 };
 </script>
